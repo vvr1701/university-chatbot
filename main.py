fb8eec938696982fc91aa0908ce2a32f31eaeb71
vishnu vardhan <vishnuvardhan1701@gmail.com>
2025-02-14 00:49:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel
 from typing import Dict
 import os
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow frontend to access backend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app = FastAPI()
 
